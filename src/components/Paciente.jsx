ce52d0b8e813ee59330b29f5c966d05347e8bed6
Pablo <pablocorrea523@gmail.com>
2022-06-23 14:43:24 -0400

    primer commit

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre:{""} <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario:{""} <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email:{""} <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta:{""} <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Síntomas:{""}{" "}
+        <span className="font-normal normal-case">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
+          et, quas laborum nam a neque aspernatur dignissimos! A praesentium
+          fugiat corporis, consectetur quis voluptatibus facilis consequatur
+          quas fuga alias ducimus.
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Paciente;
