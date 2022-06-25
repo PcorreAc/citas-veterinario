975137c20e683e2e20add170cc4a3767bdec6f0c
Pablo <pablocorrea523@gmail.com>
2022-06-24 23:40:46 -0400

    proyecto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
+  const initialState = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+  const [pacientes, setPacientes] = useState(initialState);
+  const [paciente, setPaciente] = useState({});
+
+  //Almacenar información en el localStorage
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
+    setPacientes(pacientesActualizados);
+  };
+
   return (
-    <div className="container mx-auto mt-10">
+    <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 md:flex">
-        <Formulario />
-        <ListadoPacientes />
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
       </div>
     </div>
   );
